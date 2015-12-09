8c7aa160db50db2ef206c4f6fead63204e3b0354
toophy <toophy@vip.qq.com>
2015-12-09 15:53:33 +0800

    hehe

--- main.go
+++ main.go
@@ -73,7 +73,7 @@ func (this *MasterThread) On_netEvent(m *toogo.Tmsg_net) bool {
 }
 
 // -- 当网络消息包解析出现问题, 如何处理?
-func (this *MasterThread) On_packetError(sessionId uint32) {
+func (this *MasterThread) On_packetError(sessionId uint64) {
 	toogo.CloseSession(this.Get_thread_id(), sessionId)
 }
 
@@ -82,13 +82,11 @@ func (this *MasterThread) On_registNetMsg() {
 	this.RegistNetMsg(proto.C2S_chat_Id, this.on_c2s_chat)
 }
 
-func (this *MasterThread) on_c2s_chat(pack *toogo.PacketReader, sessionId uint32) bool {
+func (this *MasterThread) on_c2s_chat(pack *toogo.PacketReader, sessionId uint64) bool {
 	msg := proto.C2S_chat{}
 	msg.Read(pack)
 
-	p := new(toogo.PacketWriter)
-	d := make([]byte, 64)
-	p.InitWriter(d)
+	p := toogo.NewPacket(128)
 
 	msgChat := new(proto.S2C_chat)
 	msgChat.Source = ""
@@ -96,15 +94,7 @@ func (this *MasterThread) on_c2s_chat(pack *toogo.PacketReader, sessionId uint32
 	msgChat.Data = msg.Data
 	msgChat.Write(p)
 
-	p.PacketWriteOver()
-	session := toogo.GetSessionById(sessionId)
-
-	m := new(toogo.Tmsg_packet)
-	m.Data = p.GetData()
-	m.Len = uint32(p.GetPos())
-	m.Count = uint16(p.Count)
-
-	toogo.PostThreadMsg(session.MailId, m)
+	toogo.SendPacket(p, sessionId)
 	return true
 }
 
@@ -113,31 +103,3 @@ func main() {
 	main_thread.Init_thread(main_thread, toogo.Tid_master, "master", 1000, 100, 10000)
 	toogo.Run(main_thread)
 }
-
-// 封包处理(大批量,或者一个网络口(代理))
-// 简化发送消息的代码
-// Lua支持消息包
-// Lua支持线程间消息
-//
-// Session上面做标记, 大包, 小包 等
-// 每种包会有不同的解包机制, 包头大小
-// 如何在accecpt时就知道这个包属于哪种包头?
-// 是一个什么消息么?
-// 比如, 一开始默认都是小包
-// 验证成功后, 根据对方资质, 变成大包
-// 或者
-// 有一个Listen侦听到的都是服务器连接, 都是大包头?
-// 其实只有gate服连接才是大包头, 其他都是小包头
-//
-// 服务器之间都是大包头, 也即是包长度4字节, 包消息数量2字节,
-// 只有gate和客户端之间才是小包头, 包长度2字节, 包消息数量1字节,
-//
-// 只有服务器和gate服的连接才会包中有包, 其他都是简单的消息处理
-//
-// session需要分清楚
-// 1. 这是什么连接(服务器,客户端)
-// 2. 服务器的连接中, 是否有gate服
-//    a. CG连接 小包
-//    b. SS连接 大包
-//    c. GS连接 混合大包
-//
